1cf90b24951bf5d74a862ae9756fbb507bc79a47
Hollenbeck <k_hollenbeck@ats-hr420-dm24.fc.ca>
2019-11-18 19:43:44 -0500

    adding multiple js files and adding a lot of js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 8889, // 3306 for windows/linux or newer macs
+    uname: "root",
+    pword: "root", // blank for windows
+    dbase: "beerstats"
+}
+
+module.exports = config;
